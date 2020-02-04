9bf6afa253b693557b564012900b6d36b73ec8d5
riede <riede@10.71.7.120>
2020-02-03 19:13:19 -0500

    CashRegister Assignment

--- Review/src/CashRegisterTester.java
+++ Review/src/CashRegisterTester.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
 
 public class CashRegisterTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		CashRegister input = new CashRegister();
+
+		 final double Sentinel = -1;
+		 Scanner in = new Scanner(System.in);
+		 
+		 System.out.println("Enter a purchase amount or -1 to stop: ");
+		 
+		  while(in.hasNextDouble()) {
+			 double itemAmount = in.nextDouble();
+			 
+			  if(itemAmount != Sentinel) {
+				  System.out.println("Enter a purchase amount or -1 to stop: ");
+				  input.recordPurchase(itemAmount);
+			  }
+			  else {
+				  break;
+			  }
+		  }
+			  
+		 System.out.println("Enter a payment amount: ");
+		 double amountReceived = in.nextDouble();
+		 
+		 input.receivePayment(amountReceived);
+		 input.giveChange();
+		 input.printChange();
+		 }
+		 
 
 	}
 
-}
+
