927997dcb8930522731cd8ba831df5265ddf09dc
riede <riede@10.71.7.120>
2020-02-03 19:12:20 -0500

    CashRegister Assignment

---
+++ Review/src/CashRegister.java
@@ -0,0 +1,31 @@
+
+public class CashRegister {
+	
+		private double purchase;
+		private double payment;
+	
+		public CashRegister() {
+			purchase = 0;
+			payment = 0;
+		}
+	
+		public void recordPurchase(double itemAmount) {
+			purchase = purchase + itemAmount;
+		}
+	
+			public void receivePayment(double amountReceived) {
+				payment = payment + amountReceived;
+			}
+			public double giveChange() {
+				return (payment - purchase);
+			}
+	
+			public void printChange() {
+			System.out.printf("Your change is: %.2f", giveChange());
+			}
+	
+	
+	
+		}
+	
+	
