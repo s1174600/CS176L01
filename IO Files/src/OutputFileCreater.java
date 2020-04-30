1b2fa4e01d056b5183e3b3a2a3238243b64326bb
riede <riede@LAPTOP-FS3EPDQF>
2020-04-30 14:17:14 -0400

    IO Files

--- IO Files/src/OutputFileCreater.java
+++ IO Files/src/OutputFileCreater.java
@@ -1,12 +1,14 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class OutputFileCreater
 {  
-   public static void main(String args[])
+   public static void main(String args[]) throws IOException
    {              
        try
        {          
@@ -29,8 +31,27 @@ public class OutputFileCreater
           
            p.close();
            y.close();
-       }
-              
+           
+           String num = null;
+           FileReader reader = new FileReader("C:\\Users\\riede\\Downloads\\numbers.txt");
+           BufferedReader reader1 = new BufferedReader(reader);
+           
+           System.out.println("Input: ");
+           while((num = reader1.readLine()) != null) {
+        	   System.out.println(num);
+           }
+           
+           reader1.close();
+           
+           String tot = null;
+           FileReader reader2 = new FileReader("C:\\Users\\riede\\Downloads\\total.txt");
+           BufferedReader reader3 = new BufferedReader(reader2);
+           
+           System.out.println();
+           System.out.println("Output: ");
+           System.out.println(num = reader3.readLine());
+           }
+       
        catch(FileNotFoundException e)
        {
            System.out.println("File not found");
